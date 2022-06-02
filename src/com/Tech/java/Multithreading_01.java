422c9e3af1be980399f04e8124412614f07e7434
Akash Rajendra Rathod <ak.hackwithakash@gmail.com>
2022-06-02 15:04:42 +0530

    start of the java course from today

--- src/com/Tech/java/Multithreading_01.java
+++ src/com/Tech/java/Multithreading_01.java
@@ -2,6 +2,27 @@ package com.Tech.java;
 
 public class Multithreading_01 {
     public static void main(String[] args) {
-        System.out.println("this is multithreadding programm : ");
+//        System.out.println("this is multithreadding programm : ");
+
+        System.out.println("hey this is the akash rathod and there has learn a programming");
+        System.out.println("hey this is the akash rathod and there has learn a programming");
+        System.out.println("hey this is the akash rathod and there has learn a programming");
+        System.out.println("hey this is the akash rathod and there has learn a programming");
+        System.out.println("hey this is the akash rathod and there has learn a programming"); System.out.println("hey this is the akash rathod and there has learn a programming");
+        System.out.println("hey this is the akash rathod and there has learn a programming"); System.out.println("hey this is the akash rathod and there has learn a programming"); System.out.println("hey this is the akash rathod and there has learn a programming"); System.out.println("hey this is the akash rathod and there has learn a programming"); System.out.println("hey this is the akash rathod and there has learn a programming"); System.out.println("hey this is the akash rathod and there has learn a programming");
+        System.out.println("hey this is the akash rathod and there has learn a programming"); System.out.println("hey this is the akash rathod and there has learn a programming"); System.out.println("hey this is the akash rathod and there has learn a programming");
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
